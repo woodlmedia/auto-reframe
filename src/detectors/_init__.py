0868f61ae627fcd9f388d82aa3fa400e3d5bb8e8
woodlmedia <aehustler12@gmail.com>
2025-06-08 17:00:57 +1000

    Create _init__.py

---
+++ src/detectors/_init__.py
@@ -0,0 +1,5 @@
+from .face_detector import FaceDetector
+from .pose_detector import PoseDetector
+from .object_detector import ObjectDetector
+
+__all__ = ['FaceDetector', 'PoseDetector', 'ObjectDetector']
